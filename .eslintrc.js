ac5a2fca7008237ac25b413f750141223c0c938c
Lawrence Siden <lsiden@gmail.com>
2017-10-10 12:06:02 -0400

    eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
         "browser": true,
         "es6": true
     },
-    "extends": "eslint:recommended",
+    "extends": ["eslint:recommended", "plugin:react/recommended"],
     "parserOptions": {
         "ecmaFeatures": {
             "experimentalObjectRestSpread": true,
@@ -37,5 +37,5 @@ module.exports = {
     },
     "globals": {
         "require": true,
-    },
+    }
 };
